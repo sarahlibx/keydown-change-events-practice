a6a32756acfab2d06d97677565d844295e8615ef
Sarah Smith <155038730+sarahlibx@users.noreply.github.com>
2024-11-27 12:31:32 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var box1 = document.querySelector(".box-1");
+var box2 = document.querySelector(".box-2");
+var box3 = document.querySelector(".box-3");
+var button = document.querySelector("button");
+
+document.addEventListener("keydown", function (e) {
+  var key = e.key;
+  if (key === "1") {
+    box1.innerText = "😸";
+  } else if (key === "2") {
+    box2.innerText = "👾";
+  } else if (key === "3") {
+    box3.innerText = "🤓";
+  }
+});
+
+document.addEventListener("click", function () {
+  box1.innerText = "1";
+  box2.innerText = "2";
+  box3.innerText = "3";
+});
